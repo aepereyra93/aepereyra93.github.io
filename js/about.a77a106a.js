874bb7107d4affdc1fe1ba15b33e62716b125db6
Agustin Pereyra <apereyra@sooft.com.ar>
2022-03-07 13:42:54 -0300

    deploy

---
+++ js/about.a77a106a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkappvuepwa"]=self["webpackChunkappvuepwa"]||[]).push([[443],{613:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(736),l={},r=(0,s.Z)(l,u,a,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.a77a106a.js.map
